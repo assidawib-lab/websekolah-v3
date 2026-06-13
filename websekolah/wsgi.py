4bc242c2102ae3dbf5b8b832881e1ac3933df70c
Bilalslb <assidawib@gmail.com>
2026-06-13 21:08:15 +0700

    up

--- websekolah/wsgi.py
+++ websekolah/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
+
+path = '/home/usernameEnte/namaProjekFolderEnte'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'myproject.settings'
+)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websekolah.settings')
-
 application = get_wsgi_application()
+
